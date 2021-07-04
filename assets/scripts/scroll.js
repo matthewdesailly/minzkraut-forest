8a5ff2cbb8429b16e2e1c66b324d07bbc90a1a70
Matthew Desailly <matthewdesaillyinc@gmail.com>
2021-07-04 14:40:49 +0700

    Menambahkan License dan Optimasi

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -2,6 +2,8 @@ const targetWorld = document.querySelector('#world');
 const targetWorldLocation = targetWorld.getBoundingClientRect().top;
 const targetCharacters = document.querySelector('#characters');
 const targetCharactersLocation = targetCharacters.getBoundingClientRect().top;
+const targetLicense = document.querySelector('#license');
+const targetLicenseLocation = targetLicense.getBoundingClientRect().top;
 const targetCreator = document.querySelector('#creator');
 const targetCreatorLocation = targetCreator.getBoundingClientRect().top;
 
@@ -15,13 +17,16 @@ $(window).scroll(function () {
         $('.target-world').addClass("on-article");
         $('.target-characters').removeClass("on-article");
         $('.target-highlight').addClass("highlight");
-    } else if ($heightScrolled >= targetCharactersLocation && $heightScrolled <= targetCreatorLocation) {
+    } else if ($heightScrolled >= targetCharactersLocation && $heightScrolled <= targetLicenseLocation) {
         $('.target-characters').addClass("on-article");
         $('.target-world').removeClass("on-article");
+        $('.target-license').removeClass("on-article");
+    } else if ($heightScrolled >= targetLicenseLocation && $heightScrolled <= targetCreatorLocation) {
+        $('.target-license').addClass("on-article");
+        $('.target-characters').removeClass("on-article");
         $('.target-creator').removeClass("on-article");
     } else if ($heightScrolled >= targetCreatorLocation) {
         $('.target-creator').addClass("on-article");
-        $('.target-world').removeClass("on-article");
-        $('.target-characters').removeClass("on-article");
+        $('.target-license').removeClass("on-article");
     }
 });
